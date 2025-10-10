71ed265ea840c56dd28c17b3e9154be921110a17
David BM <correodelnino@gmail.com>
2025-10-10 23:14:23 +0200

    feat: add skeleton loader

--- dashboard/src/features/orgs/projects/events/event-triggers/hooks/useEventTriggerPagination/useEventTriggerPagination.ts
+++ dashboard/src/features/orgs/projects/events/event-triggers/hooks/useEventTriggerPagination/useEventTriggerPagination.ts
@@ -3,6 +3,8 @@ import { useCallback, useEffect, useMemo, useState } from 'react';
 type QueryResult<TData> = {
   data: TData | undefined;
   isLoading: boolean;
+  isInitialLoading: boolean;
+  refetch?: () => Promise<unknown> | void;
 };
 
 export interface UseEventTriggerPaginationOptions<TArgs, TData = unknown[]> {
@@ -39,6 +41,8 @@ export interface UseEventTriggerPaginationResult<TData = unknown[]> {
   hasNoNextPage: boolean;
   data: TData | undefined;
   isLoading: boolean;
+  isInitialLoading: boolean;
+  refetch?: () => Promise<unknown> | void;
 }
 
 export default function useEventTriggerPagination<TArgs, TData = unknown[]>({
@@ -54,7 +58,9 @@ export default function useEventTriggerPagination<TArgs, TData = unknown[]>({
   const [offset, setOffset] = useState(0);
   const [limit, setLimit] = useState(initialLimit);
 
-  const { data, isLoading } = useQueryHook(getQueryArgs(limit, offset));
+  const { data, isLoading, isInitialLoading, refetch } = useQueryHook(
+    getQueryArgs(limit, offset),
+  );
 
   const pageLength = useMemo(() => {
     if (getPageLength) {
@@ -101,5 +107,7 @@ export default function useEventTriggerPagination<TArgs, TData = unknown[]>({
     hasNoNextPage,
     data,
     isLoading,
+    isInitialLoading,
+    refetch,
   };
 }
